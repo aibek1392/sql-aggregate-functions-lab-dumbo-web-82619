5d98afa7d3d32a62f333450190cf48f3645fca5a
Aibek Ozhorov <ozhorov@gmail.com>
2019-09-04 22:24:35 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-  "SELECT * FROM students ORDER BY  DESC LIMIT 2 "
+  "SELECT * FROM students ORDER BY gpa DESC LIMIT 2 "
 end
 
 def lowest_student_gpa
